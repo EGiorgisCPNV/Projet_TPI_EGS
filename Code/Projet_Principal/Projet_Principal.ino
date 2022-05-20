c000119e1b870ba2d7a52a484066fff21934a8ed
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2022-05-20 14:43:33 +0200

    WIP

--- Code/Projet_Principal/Projet_Principal.ino
+++ Code/Projet_Principal/Projet_Principal.ino
@@ -1,8 +1,9 @@
+
 /**
    Nom : Projet TPI 
    Version initial par: Esteban GIORGIS
    Version initial créé le: 13.05.2022
-   Dernière version le: 18.05.2022
+   Dernière version le: 20.05.2022
 **/
 
 //Librairies
@@ -17,7 +18,7 @@
 #define BACKPACK_LUMINOSITY 10 //Valeur indiquant la luminosité de l'affichage 7 segments de 0 à 15
 #define PIXELS_LUMINOSITY 100 //Valeur indiquant la luminosité des LED de l'horloge de 0 à 255
 #define WAITING_TIME 3000 //Valeur, en millisecondes, indiquant le nombre de millisecondes que va durer chaque alternance pour le premier mode d'affichage de l'affichage 7 segments
-#define MAX_CO2_ALLOWED 5000 //Le taux de CO2 maximal autorisé avant une alerte
+#define MAX_CO2_ALLOWED 1500 //Le taux de CO2 maximal autorisé avant une alerte
 
 //Variables Globales
 Adafruit_BME280 bme;//initialisation du bme
@@ -33,7 +34,7 @@ unsigned sgpStatus;
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
-bool doublePoints = false;//cette variable permet l'affichage des secondes en alternant sa valeur par true et false chaque seconde
+bool doublePoints;//cette variable permet l'affichage des secondes en alternant sa valeur par true et false chaque seconde
 int displayChoice;//Variable indiquant au programme qu'est-ce que l'affichage 7 segments doit afficher
 int alertChoice;//Variable indiquant au programme qu'elle alarme il doit utiliser dans le cas ou le taux de CO2 dépasse un seuil voulu
 int stateDisplayButton;//variable lié a l'état du bouton poussoir pour changer l'affichage 7 segments
@@ -42,6 +43,7 @@ int stateAlertButton;//variable lié a l'état du bouton poussoir pour changer l
 int reverseStateAlertButton;//variable contenant l'inverse de la variable "stateAlertButton" qui permettra ensuite de bloquer le changement d'état si l'utilisateur reste appuié sur le boutton poussoir
 int phase;//variable indiquant la phase dans laquelle on se trouve
 unsigned long timeToWait;//Cette variable permet au programme, en plus de la constante "WAITING_TIME", d'alterner les 3 affichages. À noter que le type choisi "unsigned long" permet de faire durer un maximum de temps l'alternance des 3 affichages, en effet le type "unsigned" simple permet une valeur au maximum codée sur 4 octets, contrairement au type "unsigned long" qui permet une valeur max codée sur 8 octets.
+unsigned lastSecond;//Cette variable permet de garder en mémoire pour chaque boucle effectué par la fonction "loop()" la dernière valeur retournée par la fonction "millis()"
 
 /*
  * Fonction setup
@@ -55,7 +57,6 @@ void setup() {
   pinMode(5, INPUT_PULLUP); // initialisation de la pin 5 en INPUT_PULLUP
   phase = 1;//initialisation de la variable de phase a 1
   timeToWait = WAITING_TIME;//initialisation de la variable "timeToWait" en la rendant correspondant à la durée de chaque alternance d'affichage 
-  
 
   //Partie RTC
   rtcStatus = rtc.begin();//initialisation du port de communication pour la RTC
@@ -93,14 +94,18 @@ void loop() {
   fallingEdgeDetection();//Appel de la fonction pour vérifier l'état des deux boutons poussoir
   alarmDetection();//Appel de la function qui permet d'alerter si le taux de CO2 est trop élevé
 
+  Serial.println(alertChoice);
+  
   //cette condition permet, dans le cas ou le programme est dans la phase une, d'alterner les 3 affichages
   if(phase == 1){
+   
     //Permet de faire varier le contenu de l'affichage 7 segments suivant la valeur de la variable "phase"
     if(millis() > timeToWait)
     {
       timeToWait = millis() + WAITING_TIME;//A noté que la variable timeToWait est de type unsigned donc peut stock au maximum 65000 caracte contrairement a la fonction millis() qui elle ne s'arretera jms de grandis dans qu'il n'y a pas d'interuption du programme 
+    
       displayChoice++;
-      
+            
       //Une fois que la variable "displayChoice" a dépassé 2 alors elle repasse à 0
       if(displayChoice > 2){
         displayChoice = 0;
@@ -141,44 +146,42 @@ void loop() {
  */
  int fallingEdgeDetection()
  {
-  
-    //Partie lié au bouton poussoir permettant de changer l'alarme en cas de trop haut taux de CO2
     stateAlertButton = digitalRead(5);//Lecture de la pin 5
-    if(stateAlertButton == 0){
-      displayChoice = 0;
-      }
-    
+
     //Incrémente la variable "alertChoice" à chaque clique du bouton poussoir "stateAlertButton"
     if(stateAlertButton == 0 && stateAlertButton == reverseStateAlertButton){
+      //delay(150);//Ce délai permet de bloquer le fait d'appuyer en permanence sur le bouton
         alertChoice++;
 
         //si "alertChoice" a atteint plus que 3 comme valeur alors la remet à 0
         if(alertChoice > 3){
           alertChoice = 0;
-          }
-          Serial.println(alertChoice);          
+          }      
+          
     }
     reverseStateAlertButton = !stateAlertButton;  
 
 
     //Partie liée au bouton poussoir permettant de changer le contenu de l'affichage 7 segments
     stateDisplayButton = digitalRead(3);//Lecture de la pin 3
-    if(phase == 1 && stateDisplayButton == 0){
+    
+    if(phase == 1 && stateDisplayButton == 0 && stateDisplayButton == reverseStateDisplayButton){
+
       displayChoice = -1;
+      phase = 2;
       }
     
     //Incrémente la variable "displayChoice" à chaque clique du bouton poussoir "stateDisplayButton"
-    if(stateDisplayButton == 0 && stateDisplayButton == reverseStateDisplayButton){
-        phase = 2;
+    if(stateDisplayButton == 0 && stateDisplayButton == reverseStateDisplayButton){        
+        
         displayChoice++;
-
+        
         //si "displayChoice" a atteint plus que 2 comme valeur alors la remet à 0 et retourne dans la première phase
         if(displayChoice > 2){
           phase = 1;
           displayChoice = 0;
+          timeToWait = millis() + WAITING_TIME;//Permet que l'affichage lié à la valeur de la variable "displayChoice" numéro 0 puisse durer le temps voulu complet
           }
-        Serial.println(displayChoice);
-        //Serial.println(phase);
     }
     
     reverseStateDisplayButton = !stateDisplayButton;
@@ -207,9 +210,9 @@ int getCurrentTime(){
  * Description: Cette fonction affichage sur l'affichage 7 segments l'heure reçu par la RTC
 */
 void displayCurrentTime(){
-  
-  float nextDisplay = millis() + 1000;//Variable indiquant la durée ou les double points des secondes doivent rester allumés ou éteints
-  
+  doublePoints = false;//De base les double point des secondes sont éteintes
+  float doublePointDuration= millis() + 200;//Variable indiquant la durée ou les doubles points des secondes doivent rester allumée
+
   backPackDisplay.print(getCurrentTime());//Prépare l'affichage de l'horraire actuel sur l'affichage 7 segments
 
   //ajoute un 0 au digit précédent dans le cas ou la valeur des heures et/ou des minutes sont en dessous de 10
@@ -220,21 +223,22 @@ void displayCurrentTime(){
     }
   }
 
-  doublePoints = !doublePoints;//Inverse la valeur de la variable "doublePoints" pour l'affichage des deux petits points 
-  backPackDisplay.drawColon(doublePoints);//Prépare l'affichage ou non des deux petits point selon la valeur de la variable "doublePoints"
 
-  
-  //cette boucle while permet d'allumer ou éteindre les 2 boutons chaque seconde
-  while(millis() < nextDisplay){
-    fallingEdgeDetection();
-    alarmDetection();//Appel de la function qui permet d'alerter si le taux de CO2 est trop élevé
-    
-      backPackDisplay.writeDisplay();//Affiche toutes les informations préparées au préalable sur l'affichage 7 segments
-
-      if(millis() == nextDisplay){
-        break;  
-      }
+  //cette boucle while permet d'allumer les doubles points des secondes chaque seconde durant un certain temps
+  if(seconds - lastSecond != 0){
+        while(millis() < doublePointDuration){
+        doublePoints = true;//Inverse la valeur de la variable "doublePoints" pour l'affichage des deux petits points 
+        backPackDisplay.drawColon(doublePoints);//Prépare l'affichage ou non des deux petits point selon la valeur de la variable "doublePoints"
+        backPackDisplay.writeDisplay();//Affiche toutes les informations préparées au préalable sur l'affichage 7 segments
+        lastSecond = seconds;
+        if(millis() == doublePointDuration){
+           break;  
+        }
+    }
   }
+
+  backPackDisplay.drawColon(doublePoints);//Prépare l'affichage ou non des deux petits point selon la valeur de la variable "doublePoints"
+  backPackDisplay.writeDisplay();//Affiche toutes les informations préparées au préalable sur l'affichage 7 segments
 }
 
 
@@ -280,7 +284,7 @@ float getCOTwoRate(){
 void alarmDetection()
 {
   sgp.IAQmeasure();//demande au capteur de mesurer une seul mesure de eCO2 et de VOC. Place ensuite les mesures dans TVOC et eCO2
-
+  
   //Tant que le taux de CO2 mesuré dépasse la valeur de la constante "MAX_CO2_ALLOWED" il effectue une alarme spécifique
   while(sgp.eCO2 >= MAX_CO2_ALLOWED){
     
@@ -308,7 +312,7 @@ void alarmDetection()
       }
 
       sgp.IAQmeasure();//Reprise de mesure
-      
+
       if(sgp.eCO2 < MAX_CO2_ALLOWED){
         break;  
       }
@@ -320,11 +324,13 @@ void alarmDetection()
  * Description: Cette fonction affichage sur l'affichage 7 segments le taux de CO2 mesurée par le capteur SGP30 
 */
 void displayCOTwoRate(){
-      
+
   backPackDisplay.print(getCOTwoRate());//Prépare l'affichage du taux de CO2 sur l'affichage 7 segments
   delay(25);//ajout d'un délai pour avoir un affichage plus agréable est ne pas rendre l'affichage 7 segments illisible à cause de la trop grande variation de prise de mesure du taux de CO2 par le capteur SGP30
   backPackDisplay.writeDisplay();//Affiche tous ce qui a été préparé 
+
 }
+
 
 
 /*
@@ -366,21 +372,38 @@ void turnOffClockLED(){
  * Description: Cette fonction fait clignoter les 60 LED de l'horloge 60 LED, elle servira comme alerte visuelle en cas de trop haut taux de CO2
 */
 void visualAlertForCOTwoRate(){
+
+  float nextDisplay = millis() + 100;
   
-  //initialise chaque LED avec la couleur rouge
-  for(int i=0; i<NUMPIXELS; i++) {
+  //cette boucle while permet d'allumer chaque LED et de produire du son avec le buzzer durant 100 millisecondes
+  while(millis() < nextDisplay){
+    displayCOTwoRate();
+      for(int i=0; i<NUMPIXELS; i++) {
     ledClock.setPixelColor(i, ledClock.Color(150, 0, 0));
   }
   ledClock.show();//Affiche les couleurs précédemment initialisées
-  delay(100);
-  
 
-  //initialise chaque LED à 0 donc sans couleurs
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+    if(millis() == nextDisplay){
+        break;  
+      }
+  }
+
+
+
+  float secondNextDisplay = nextDisplay + 100;//Variable indiquant la durée ou les double points des secondes doivent rester allumés ou éteints
+
+  //cette boucle while permet d'allumer chaque LED et de produire du son avec le buzzer durant 100 millisecondes
+  while(millis() < secondNextDisplay){
+    displayCOTwoRate();//Appel de la fonction permettant d'afficher le taux de CO2
+      for(int i=0; i<NUMPIXELS; i++) {
     ledClock.setPixelColor(i, ledClock.Color(0, 0, 0));
   }
   ledClock.show();//Affiche les couleurs précédemment initialisées
-  delay(100);
+
+    if(millis() >= secondNextDisplay){
+        break;  
+      }
+  }
 }
 
 
@@ -388,11 +411,29 @@ void visualAlertForCOTwoRate(){
  * Description: Cette fonction fait sonner le buzzer en alternance, elle servira comme alerte sonore en cas de trop haut taux de CO2
 */
 void sonorAlertForCOTwoRate(){
+  float nextDisplay = millis() + 100;
+  
+  //cette boucle while permet d'allumer chaque LED et de produire du son avec le buzzer durant 100 millisecondes
+  while(millis() < nextDisplay){
+    displayCOTwoRate();//Appel de la fonction permettant d'afficher le taux de CO2
+     tone(4, 100); //Envoie 100 KHz de signal de son
 
-  tone(4, 100); //Envoie 100 KHz de signal de son
-  delay(5);
-  noTone(4);//Stop le buzzer
-  delay(1000);
+    if(millis() >= nextDisplay){
+        break;  
+      }
+  }
+
+  float secondNextDisplay = nextDisplay + 100;
+  
+  //cette boucle while permet d'allumer chaque LED et de produire du son avec le buzzer durant 100 millisecondes
+  while(millis() < secondNextDisplay){
+    displayCOTwoRate();//Appel de la fonction permettant d'afficher le taux de CO2
+    noTone(4); //Stop le buzzer
+
+    if(millis() >= secondNextDisplay){
+        break;  
+      }
+  }
   
 }
 
@@ -401,12 +442,12 @@ void sonorAlertForCOTwoRate(){
  * Description: Cette fonction fait clignoter les 60 LED de l'horloge 60 LED et faire "clignoter" le buzzer avec du son, elle servira comme alerte visuelle et sonore en cas de trop haut taux de CO2
 */
 void doubleAlert(){
-     float nextDisplay = millis() + 100;//Variable indiquant la durée ou les double points des secondes doivent rester allumés ou éteints
-  float secondNextDisplay = nextDisplay + 100;//Variable indiquant la durée ou les double points des secondes doivent rester allumés ou éteints
-
-
+  float nextDisplay = millis() + 100;
+  
   //cette boucle while permet d'allumer chaque LED et de produire du son avec le buzzer durant 100 millisecondes
   while(millis() < nextDisplay){
+    displayCOTwoRate();//Appel de la fonction permettant d'afficher le taux de CO2
+    
     //initialise chaque LED avec la couleur rouge
     for(int i=0; i<NUMPIXELS; i++) {
       ledClock.setPixelColor(i, ledClock.Color(150, 0, 0));
@@ -414,14 +455,16 @@ void doubleAlert(){
     ledClock.show();//Affiche les couleurs précédemment initialisées
     tone(4, 100); //Envoie 100 KHz de signal de son
     if(millis() == nextDisplay){
-      
         break;  
       }
   }
   
-
+  float secondNextDisplay = nextDisplay + 100;
+  
   //cette boucle while permet d'éteindre chaque LED et d'éteindre le buzzer durant 100 millisecondes
   while(millis() < secondNextDisplay){
+  displayCOTwoRate();//Appel de la fonction permettant d'afficher le taux de CO2
+    
     //initialise chaque LED à 0 donc éteinte
     for(int i=0; i<NUMPIXELS; i++) {
       ledClock.setPixelColor(i, ledClock.Color(0, 0, 0));
@@ -429,7 +472,7 @@ void doubleAlert(){
     }
     ledClock.show();//Affiche les couleurs précédemment initialisées
     noTone(4);//Stop le buzzer
-    if(millis() == nextDisplay + 100){
+    if(millis() >= secondNextDisplay){
         break;  
       }
   }
